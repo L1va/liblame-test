d6e701d240f55cbb4547ed3336d386c25663a08c
Mike Ivanou <mi@gojuno.com>
2016-08-10 10:01:51 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+Fork of
+https://github.com/intervigilium/liblame
+and used with gradle.
---
+++ app/src/main/java/com/example/l1va/liblame/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.l1va.liblame;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity{
+}
